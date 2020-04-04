6c091c5da8507c8f9086984bdb184772d5694287
Julie T <joules2319@gmail.com>
2020-04-03 23:39:37 -0600

    renamed cache variables. adjusted functions to be more like the examples in pwa activities.

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,45 +11,49 @@ const FILES_TO_CACHE = [
 ];
 
 //
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const STATIC_CACHE = "static-cache-v2";
+const RUNTIME_CACHE = "data-cache-v1";
 
 // install files
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(STATIC_CACHE).then(cache => {
       console.log("Your files were pre-cached successfully!");
       return cache.addAll(FILES_TO_CACHE);
-    }).catch(err => console.log(err))
+    }).then(() => self.skipWaiting()).catch(err => console.log(err))
   );
 
-  self.skipWaiting();
+  // self.skipWaiting();
 });
 
-// activate
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
-      ).catch(err => console.log(err))
-        ;
-    })
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", event => {
+  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+        // return array of cache names that are old to delete
+        return cacheNames.filter(
+          cacheName => !currentCaches.includes(cacheName)
+        );
+      })
+      .then(cachesToDelete => {
+        return Promise.all(
+          cachesToDelete.map(cacheToDelete => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
   );
-
-  self.clients.claim();
 });
 
 // fetch
 self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
+      caches.open(RUNTIME_CACHE).then(cache => {
         return fetch(evt.request)
           .then(response => {
             // If the response was good, clone it and store it in the cache.
@@ -63,17 +67,18 @@ self.addEventListener("fetch", function (evt) {
             // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
-      }).catch(err => console.log(err))
+      })
     );
 
     return;
   }
 
   evt.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.match(evt.request).then(response => {
-        return response || fetch(evt.request);
-      });
+    caches.open(STATIC_CACHE).then(cache => {
+      return fetch(evt.request).then(response => {
+        cache.put(evt.request, response.clone());
+        return response;
+      }).catch(() => caches.match(evt.request))
     })
   );
 });
